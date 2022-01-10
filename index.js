5cd338bc81aa49ea02ca5b6707f2120666d03aa7
Jeremy Levy <jje.levy@gmail.com>
2022-01-10 17:20:59 +0100

    Finish 3.19: Phonebook database, step7

--- index.js
+++ index.js
@@ -102,6 +102,11 @@ app.get('/info', (request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
   console.error(error)
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
   next(error)
 }
 app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -11,7 +12,10 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    unique: true
+  },
   number: String
 })
 
@@ -22,5 +26,7 @@ personSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
+
+personSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', personSchema)
